5a713e688a4251178bcaa416c225a850452963e2
Kiwi <84627732+Kiwious@users.noreply.github.com>
2022-01-09 22:01:31 +0100

    Update error.py

--- error.py
+++ error.py
@@ -1,20 +1,20 @@
 import discord
 from discord.ext import commands
 
- class CommandErrorHandler(commands.Cog):
+class CommandErrorHandler(commands.Cog):
 
-     def __init__(self, client):
-         self.client = client
+    def __init__(self, client):
+        self.client = client
 
-     @commands.Cog.listener()
-     async def on_command_error(self, ctx, error):
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
 
-         if isinstance(error, commands.CommandError):
+        if isinstance(error, commands.CommandError):
 
-             embed = discord.Embed(title="Fehler!", color=0xff6961, description=f"```{error}```")
-             embed.set_footer(text="Um den Fehler zu reporten, wende dich bitte an das Serverteam.")
+            embed = discord.Embed(title="Fehler!", color=0xff6961, description=f"```{error}```")
+            embed.set_footer(text="Um den Fehler zu reporten, wende dich bitte an das Serverteam.")
 
-             await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
 
- def setup(client):
-     client.add_cog(CommandErrorHandler(client)) 
+def setup(client):
+    client.add_cog(CommandErrorHandler(client)) 

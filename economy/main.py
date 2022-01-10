f601ebd9510c2d09fdabc1fc81dbe524e0eea7f7
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-01-10 18:26:09 +0100

    Added withdraw command

--- economy/main.py
+++ economy/main.py
@@ -22,11 +22,21 @@ async def get_bank_data():
         users = json.load(f)
     return users
 
+async def update_bank(user, change=0, mode='wallet'):
+    users = await get_bank_data()
+    users[str(user.id)][mode] += change
+
+    with open('bank.json', 'r') as f:
+        json.dump(users, f)
+
+    bal = [users[str(user.id)]['wallet'], users[str(user.id)]['bank']]
+    return bal
+
 class EconomyManager(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    @commands.command(aliases=['bal'])
     async def balance(self, ctx):
 
         user = ctx.author
@@ -38,8 +48,8 @@ class EconomyManager(commands.Cog):
         bank_amt = users[str(user.id)]['bank']
 
         embed = discord.Embed(title=f'<a:bewegendeszeichenlmao:920059343108452353> Kontostand von {ctx.author}', color=0x415fe6)
-        embed.add_field(name='Geld', value=f'**{wallet_amt}** 🐚', inline=False)
-        embed.add_field(name='Bank', value=f'**{bank_amt}** 🐚', inline=False)
+        embed.add_field(name='Geld', value=f'**{wallet_amt}**🐚', inline=False)
+        embed.add_field(name='Bank', value=f'**{bank_amt}**🐚', inline=False)
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -57,12 +67,33 @@ class EconomyManager(commands.Cog):
         wallet_amt = users[str(user.id)]['wallet']
         users[str(user.id)]['wallet'] += income
 
-
-
         with open('bank.json', 'w') as f:
             json.dump(users, f, indent=4)
+    
+    @commands.command(aliases=['with'])
+    async def withdraw(self, ctx, amount=None):
+        await open_account(ctx.author)
 
+        if amount == None:
+            embed = discord.Embed(color=0x415fe6, description='<a:bewegendeszeichenlmao:920059343108452353> Bitte gib einen Betrag an, der von deiner Bank abgehoben werden soll.')
+            return await ctx.send(embed=embed)
 
+        bal = await update_bank(user=ctx.author)
+
+        amount = int(amount)
+        if amount > bal[1]:
+            embed1 = discord.Embed(color=0x415fe6, description='<a:bewegendeszeichenlmao:920059343108452353> Du hast nicht genügend Geld auf der Bank.')
+            return await ctx.send(embed=embed1)
+        
+        if amount < 0:
+            embed2 = discord.Embed(color=0x415fe6, description='<a:bewegendeszeichenlmao:920059343108452353> Du kannst keinen negativen Betrag abheben.')
+            return await ctx.send(embed=embed2)
+        
+        await update_bank(ctx.author, amount, 'wallet')
+        await update_bank(ctx.author, -1*amount, 'bank')
+
+        embed3 = discord.Embed(color=0x415fe6, title='<a:bewegendeszeichenlmao:920059343108452353> Erfolgreich abgehoben!', description=f'Du hast erfolgreich **{amount}**🐚 von deiner Bank abgehoben.')
+        await ctx.send(embed=embed3)
 
 def setup(client):
     client.add_cog(EconomyManager(client))

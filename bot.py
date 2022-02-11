db239ff4ae990278a77edff6909b72447af04425
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-02-11 16:41:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
-from listeners.rainbow_role import *
+from cogs.rainbow_role import *
 
 client = commands.Bot(command_prefix=',')
 
